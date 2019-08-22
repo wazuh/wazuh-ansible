e1b084c1a7175b930909eb9f9cada1188ecf80a7
Rshad Zhran <rashzk95@gmail.com>
2019-08-22 16:26:07 +0200

    Adding hash_behaviour: merge in order not to override the default variables

--- run_none_cluster.sh
+++ run_none_cluster.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+      echo "Platform not selected. Please select a platform. => Aborting"
+      exit
+else
+      cp Pipfile.template Pipfile
+      sed -i "s/_PLATFORM_/$1/g" Pipfile
+fi
+
 sudo pipenv run elasticsearch
 sudo pipenv run test
 sudo pipenv run agent
 sudo pipenv run kibana
+
+cp Pipfile.template Pipfile
