9755281a2f71b7024c3a6a16934acf83c0ef6292
Rshad Zhran <rashzk95@gmail.com>
2019-08-26 21:43:27 +0200

    adapting run scirpt

--- run_cluster_mode.sh
+++ run_cluster_mode.sh
@@ -4,15 +4,15 @@ paths=( "molecule/default/" "molecule/worker/" "molecule/elasticsearch/" "molecu
 images=( "solita/ubuntu-systemd:bionic" "solita/ubuntu-systemd:xenial" "milcom/centos7-systemd" "ubuntu:trusty" "centos:6" )
 platform=( "bionic" "xenial" "centos7" "trusty" "centos6" )
 
-echo "Please select an image. "
-
-select IMAGE in "${images[@]}";
-do
-     echo "You picked $IMAGE ($REPLY)"
-     break
-done
-
-index=$(($REPLY - 1))
+#echo "Please select an image. "
+#
+#select IMAGE in "${images[@]}";
+#do
+#     echo "You picked $IMAGE ($REPLY)"
+#     break
+#done
+#
+#index=$(($REPLY - 1))
 
 if [ -z "$IMAGE" ]
 then
@@ -22,11 +22,11 @@ else
         for i in "${paths[@]}"
         do
             cp "$i/playbook.yml.template" "$i/playbook.yml"
-            sed -i "s/platform/${platform[$index]}/g" "$i/playbook.yml"
+            sed -i "s/platform/${platform[0]}/g" "$i/playbook.yml"
 
             cp "$i/molecule.yml.template" "$i/molecule.yml"
-            sed -i "s|imagename|${images[$index]}|g" "$i/molecule.yml"
-            sed -i "s/platform_/${platform[$index]}/g" "$i/molecule.yml"    
+            sed -i "s|imagename|${images[0]}|g" "$i/molecule.yml"
+            sed -i "s/platform_/${platform[0]}/g" "$i/molecule.yml"    
 
         done
 fi
