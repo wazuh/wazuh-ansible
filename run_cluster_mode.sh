b2a204b5a46535ff4b4c4b85f0046dcdd7a177dc
Rshad Zhran <rashzk95@gmail.com>
2019-09-09 13:53:32 +0200

    returning to the interactive version of the script run_cluster_mode.sh

--- run_cluster_mode.sh
+++ run_cluster_mode.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
 paths=( "molecule/default/" "molecule/worker/" "molecule/elasticsearch/" "molecule/kibana/" )
-images=( "milcom/centos7-systemd" "solita/ubuntu-systemd:bionic" "solita/ubuntu-systemd:xenial" )
-platform=( "centos7" "bionic" "xenial" )
-#    
-#echo "Please select an image. "
-#
-#select IMAGE in "${images[@]}";
-#do
-#     echo "You picked $IMAGE ($REPLY)"
-#     break
-#done
-#
-#index=$(($REPLY - 1))
-#
-#if [ -z "$IMAGE" ]
-#then
-#      echo "Platform not selected. Please select a platform of [bionuc, xenial or centos7]. => Aborting"
-#      exit
-#else
-for index in "${!images[@]}"
+images=( "solita/ubuntu-systemd:bionic" "solita/ubuntu-systemd:xenial" "milcom/centos7-systemd" "ubuntu:trusty" "centos:6" )
+platform=( "bionic" "xenial" "centos7" "trusty" "centos6" )
+
+echo "Please select an image. "
+
+select IMAGE in "${images[@]}";
 do
-    echo $index
-    for i in "${paths[@]}"
-    do
-        #echo $i
-        echo ${platform[$index]}
-        echo ${images[$index]}
-        cp "$i/playbook.yml.template" "$i/playbook.yml"
-        sed -i "s/platform/${platform[$index]}/g" "$i/playbook.yml"
-
-        cp "$i/molecule.yml.template" "$i/molecule.yml"
-        sed -i "s|imagename|${images[$index]}|g" "$i/molecule.yml"
-        sed -i "s/platform_/${platform[$index]}/g" "$i/molecule.yml"    
-    done
-
-    sudo pipenv run elasticsearch
-    sudo pipenv run test
-    sudo pipenv run worker
-    sudo pipenv run kibana
-
-    sudo pipenv run destroy
-    sudo pipenv run destroy_worker
-    sudo pipenv run destroy_kibana
-    sudo pipenv run destroy_elasticsearch
+     echo "You picked $IMAGE ($REPLY)"
+     break
 done
-#fi
+
+index=$(($REPLY - 1))
+
+if [ -z "$IMAGE" ]
+then
+      echo "Platform not selected. Please select a platform of [bionuc, xenial or centos7]. => Aborting"
+      exit
+else
+        for i in "${paths[@]}"
+        do
+            cp "$i/playbook.yml.template" "$i/playbook.yml"
+            sed -i "s/platform/${platform[$index]}/g" "$i/playbook.yml"
+
+            cp "$i/molecule.yml.template" "$i/molecule.yml"
+            sed -i "s|imagename|${images[$index]}|g" "$i/molecule.yml"
+            sed -i "s/platform_/${platform[$index]}/g" "$i/molecule.yml"    
+
+        done
+fi
+
+sudo pipenv run elasticsearch
+sudo pipenv run test
+sudo pipenv run worker
+sudo pipenv run kibana
+
+sudo pipenv run destroy
+sudo pipenv run destroy_worker
+sudo pipenv run destroy_kibana
+sudo pipenv run destroy_elasticsearch
 
