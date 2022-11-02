53e1e614534388d44f443908c64c4001edffad18
Gonzalo Acuña <teddytpc1@gmail.com>
2022-11-02 14:23:35 -0300

    Changes to include the packages_repository variable

--- roles/wazuh/check-packages/scripts/check_packages.sh
+++ roles/wazuh/check-packages/scripts/check_packages.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 #echo $VERSION
 ## Replace VERSION with $VERSION in packages_uri.txt and save it as packages_uri_new.txt
-sed 's,VERSION,'$VERSION',g' packages_uri.txt > packages_uri_new.txt
+sed 's,VERSION,'$VERSION',g' ../files/packages_uri.txt > ../files/packages_uri_new.txt
 
 checkPackages(){
     ## Set S3 Bucket URL
@@ -21,7 +21,7 @@ checkPackages(){
     ## Loop through the packages_uri_new.txt file
     while IFS= read -r URI
     do
-        #echo "$URI"
+        echo "$URI"
         ## Check if the package exists
         PACKAGE=$(curl --silent -I $PACKAGES_URL$URI | grep -E "^HTTP" | awk  '{print $2}')
         ## If it does not exist set EXISTS to 1 (false)
@@ -30,20 +30,28 @@ checkPackages(){
             #echo $PACKAGES_URL$URI "does not exist"
             return $EXISTS
         fi
-    done < packages_uri_new.txt
+    done < ../files/packages_uri_new.txt
 
     return $EXISTS
+}
+
+replaceVars(){
+    sed -i "s|packages_repository:.*|packages_repository: $1|g" ../../vars/repo_vars.yml
+
 }
 
 ## Call the checkPackages function for each repository
 if checkPackages "production"; then
     echo "production"
+    replaceVars "production"
     exit 0
 elif checkPackages "pre-release"; then
     echo "pre-release"
+    replaceVars "pre-release"
     exit 0
 elif checkPackages "staging"; then
     echo "staging"
+    replaceVars "staging"
     exit 0
 else
     echo "Failed"
